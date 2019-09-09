5aab073b7951e09e1ef0d6d65e6064d92fb03a6c
ReemWSW <safefire@gmail.com>
2019-09-09 11:11:06 +0700

    การใช้งานข้อมูลชนิด boolean

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -10,6 +10,27 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            //สร้างตัวแปร Random โดยการมีสุ่มเลข 1 หลัก (0–9)
+            Random r = new Random();
+            int rd = r.Next(0, 10);
+            Console.WriteLine($"Random number : {rd}");
+
+            //สร้างตัวแปรชนิด integer สำหรับรับค่าจากผู้ใช้
+            Console.Write("Enter number : ");
+            int n = int.Parse(Console.ReadLine());
+
+            //สร้างตัวแปร boolean โดยเก็บค่าที่ได้จากการเปรียบเทียบตัวเลขในข้อ 1 และ 2
+            bool b;
+
+            if (rd == n)
+                b = true;
+            else
+                b = false;
+
+            //ให้พิมพ์ค่าตัวแปร boolean ในข้อ 3 ออกทางหน้าจอ
+            Console.WriteLine($"Random Equal input : {b}");
+
+
         }
     }
 }
