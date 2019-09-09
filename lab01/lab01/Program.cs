da1188bdaf2bfd62f746a1bc158f1a305044ee0e
ReemWSW <safefire@gmail.com>
2019-09-09 10:57:02 +0700

    การรับ string จากคีย์บอร์ด

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -10,6 +10,10 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            string str;
+            Console.Write("Enter some characters.");
+            str = Console.ReadLine();
+            Console.WriteLine("You entered: " + str);
         }
     }
 }
