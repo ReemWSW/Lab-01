50dd02212f3aa385f14957d05496a69a420c4a0d
ReemWSW <safefire@gmail.com>
2019-09-09 11:30:47 +0700

    การใช้เครื่องหมายทางคณิตศาสตร์กับตัวแปรชนิดจำนวนเต็ม

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -10,6 +10,13 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            int a, b, c, d, e, f;
+            a = 1;
+            b = a + 6;
+            c = b - 3;
+            d = c * 2;
+            e = d / 2;
+            f = e % 2;
         }
     }
 }
