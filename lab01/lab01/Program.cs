9a5b8fce28ded43fa4fda1d2afffe060a4ed1b3f
ReemWSW <safefire@gmail.com>
2019-09-09 10:55:20 +0700

    การรับตัวอักษรจากคีย์บอร์ด

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -10,6 +10,10 @@ namespace lab01
     {
         static void Main(string[] args)
         {
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);
         }
     }
 }
