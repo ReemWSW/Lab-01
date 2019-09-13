8ab3de5be706c3325f61144235971f8c13c8e9c9
ReemWSW <safefire@gmail.com>
2019-09-14 05:50:18 +0700

    วาดคลื่น y = x2

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -10,9 +10,9 @@ namespace lab01
     {
         static void Main(string[] args)
         {
-            for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
+            for (float i = -3; i < Math.PI ; i += 0.3F)
             {
-                Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" + spaces(Math.Sin(i)) + "*", i, Math.Sin(i));
+                Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" + spaces(Math.Pow(i, 2)) + "*", i, Math.Pow(i, 2));
             }
         }
         private static string spaces(double val)
